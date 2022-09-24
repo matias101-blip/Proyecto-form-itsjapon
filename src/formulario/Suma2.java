f69dfd70c145abdb58d87555fa9f78324e4cb6d0
thehunter101 <murciamatias101@gmail.com>
2022-09-24 11:57:00 -0500

    Mi primer commit al projecto

--- src/formulario/Suma2.java
+++ src/formulario/Suma2.java
@@ -4,16 +4,18 @@
  */
 package formulario;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Matias
  */
-public class notas extends javax.swing.JFrame {
+public class Suma2 extends javax.swing.JFrame {
 
     /**
-     * Creates new form notas
+     * Creates new form Suma2
      */
-    public notas() {
+    public Suma2() {
         initComponents();
     }
 
@@ -26,27 +28,39 @@ public class notas extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lblt = new javax.swing.JLabel();
         lbln1 = new javax.swing.JLabel();
         lbln2 = new javax.swing.JLabel();
-        lbln3 = new javax.swing.JLabel();
         txtn1 = new javax.swing.JTextField();
         txtn2 = new javax.swing.JTextField();
-        txtn3 = new javax.swing.JTextField();
+        lblr = new javax.swing.JLabel();
+        txtr = new javax.swing.JTextField();
         btnc = new javax.swing.JButton();
         btnl = new javax.swing.JButton();
         btns = new javax.swing.JButton();
-        btnp = new javax.swing.JButton();
-        txtprom = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        lbln1.setText("Nota 1");
+        lblt.setText("Suma de dos numeros");
 
-        lbln2.setText("Nota 2");
+        lbln1.setText("Ingrese el primer numero");
 
-        lbln3.setText("Nota 3");
+        lbln2.setText("Ingrese el segundo numero");
 
-        btnc.setText("Calcular");
+        txtn1.setText("Escriba un numero mayorque 0");
+        txtn1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtn1ActionPerformed(evt);
+            }
+        });
+
+        txtn2.setText("Escriba un numero mayorque 0");
+
+        lblr.setText("Resultado");
+
+        txtr.setEditable(false);
+
+        btnc.setText("Calculadora");
         btnc.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btncActionPerformed(evt);
@@ -67,70 +81,58 @@ public class notas extends javax.swing.JFrame {
             }
         });
 
-        btnp.setText("Promedio");
-
-        txtprom.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtpromActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(33, 33, 33)
+                .addGap(50, 50, 50)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btnc)
-                            .addComponent(btnp, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(33, 33, 33)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(btnl)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 70, Short.MAX_VALUE)
-                                .addComponent(btns))
-                            .addComponent(txtprom))
-                        .addGap(21, 21, 21))
+                        .addComponent(btnc)
+                        .addGap(42, 42, 42)
+                        .addComponent(btnl)
+                        .addGap(29, 29, 29)
+                        .addComponent(btns))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lbln1)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(lblr)
                             .addComponent(lbln2)
-                            .addComponent(lbln3))
-                        .addGap(69, 69, 69)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(txtn1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE)
-                            .addComponent(txtn2, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtn3))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addComponent(lbln1))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtn2, javax.swing.GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
+                            .addComponent(txtn1)
+                            .addComponent(txtr))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(lblt)
+                .addGap(146, 146, 146))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(24, 24, 24)
+                .addGap(11, 11, 11)
+                .addComponent(lblt)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbln1)
                     .addComponent(txtn1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(26, 26, 26)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbln2)
                     .addComponent(txtn2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(32, 32, 32)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbln3)
-                    .addComponent(txtn3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(28, 28, 28)
+                    .addComponent(lblr)
+                    .addComponent(txtr, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(32, 32, 32)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnc)
                     .addComponent(btnl)
                     .addComponent(btns))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnp)
-                    .addComponent(txtprom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(60, Short.MAX_VALUE))
+                .addContainerGap(98, Short.MAX_VALUE))
         );
 
         pack();
@@ -138,25 +140,44 @@ public class notas extends javax.swing.JFrame {
 
     private void btncActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btncActionPerformed
         // TODO add your handling code here:
-        double n1 = Double.parseDouble(txtn1.getText());
-        double n2 = Double.parseDouble(txtn2.getText());
-        double n3 = Double.parseDouble(txtn3.getText());
-        double p1=0;
-        p1= (n1+n2+n3) / 3;
-        txtprom.setText(Double.toString(p1));
+        int n1, n2, sum;
+        do{
+            n1 = Integer.parseInt(txtn1.getText());
+            if (n1 > 0){
+                JOptionPane.showMessageDialog(null,"El numero Ingresado es el correcto");
+            }else{
+                JOptionPane.showMessageDialog(null, "EL numero ingresado no es el correcto");
+                txtn1.setText("Escriba un numero mayor que 0");
+                txtn1.grabFocus();
+            }
+        }while(n1<=0);
+        
+        do{
+           n2 = Integer.parseInt(txtn2.getText());
+            if (n2 > 0){
+                JOptionPane.showMessageDialog(null,"El numero Ingresado es el correcto");
+            }else{
+                JOptionPane.showMessageDialog(null, "EL numero ingresado no es el correcto");
+                txtn2.setText("Escriba un numero mayor que 0");
+                txtn2.grabFocus();
+            }
+        }while (n2<=0);
+        sum = n1 + n2;
+        
+        txtr.setText("La respuesta es: " + sum);
     }//GEN-LAST:event_btncActionPerformed
 
-    private void txtpromActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtpromActionPerformed
+    private void txtn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtn1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_txtpromActionPerformed
+        
+    }//GEN-LAST:event_txtn1ActionPerformed
 
     private void btnlActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnlActionPerformed
         // TODO add your handling code here:
         txtn1.setText("");
         txtn2.setText("");
-        txtn3.setText("");
-        txtprom.setText("");
-        txtn1.grabFocus();
+        txtr.setText("");
+        txtr.grabFocus();
     }//GEN-LAST:event_btnlActionPerformed
 
     private void btnsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnsActionPerformed
@@ -181,20 +202,20 @@ public class notas extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(notas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Suma2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(notas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Suma2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(notas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Suma2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(notas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Suma2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new notas().setVisible(true);
+                new Suma2().setVisible(true);
             }
         });
     }
@@ -202,14 +223,13 @@ public class notas extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnc;
     private javax.swing.JButton btnl;
-    private javax.swing.JButton btnp;
     private javax.swing.JButton btns;
     private javax.swing.JLabel lbln1;
     private javax.swing.JLabel lbln2;
-    private javax.swing.JLabel lbln3;
+    private javax.swing.JLabel lblr;
+    private javax.swing.JLabel lblt;
     private javax.swing.JTextField txtn1;
     private javax.swing.JTextField txtn2;
-    private javax.swing.JTextField txtn3;
-    private javax.swing.JTextField txtprom;
+    private javax.swing.JTextField txtr;
     // End of variables declaration//GEN-END:variables
 }
