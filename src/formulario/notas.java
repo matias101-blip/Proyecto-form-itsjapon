bc23fa42c79905ebb1804ee36d668bfc38af4f0d
thehunter101 <murciamatias101@gmail.com>
2022-09-24 10:45:02 -0500

    Mi programa para calcular notas ya esta desarrollado

--- src/formulario/notas.java
+++ src/formulario/notas.java
@@ -26,21 +26,133 @@ public class notas extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lbln1 = new javax.swing.JLabel();
+        lbln2 = new javax.swing.JLabel();
+        lbln3 = new javax.swing.JLabel();
+        txtn1 = new javax.swing.JTextField();
+        txtn2 = new javax.swing.JTextField();
+        txtn3 = new javax.swing.JTextField();
+        btnc = new javax.swing.JButton();
+        btnl = new javax.swing.JButton();
+        btns = new javax.swing.JButton();
+        btnp = new javax.swing.JButton();
+        txtprom = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lbln1.setText("Nota 1");
+
+        lbln2.setText("Nota 2");
+
+        lbln3.setText("Nota 3");
+
+        btnc.setText("Calcular");
+        btnc.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btncActionPerformed(evt);
+            }
+        });
+
+        btnl.setText("Limpiar");
+        btnl.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnlActionPerformed(evt);
+            }
+        });
+
+        btns.setText("Salir");
+
+        btnp.setText("Promedio");
+
+        txtprom.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtpromActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnc)
+                            .addComponent(btnp, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(33, 33, 33)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnl)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 70, Short.MAX_VALUE)
+                                .addComponent(btns))
+                            .addComponent(txtprom))
+                        .addGap(21, 21, 21))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lbln1)
+                            .addComponent(lbln2)
+                            .addComponent(lbln3))
+                        .addGap(69, 69, 69)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(txtn1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE)
+                            .addComponent(txtn2, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtn3))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbln1)
+                    .addComponent(txtn1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbln2)
+                    .addComponent(txtn2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbln3)
+                    .addComponent(txtn3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnc)
+                    .addComponent(btnl)
+                    .addComponent(btns))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnp)
+                    .addComponent(txtprom, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(60, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btncActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btncActionPerformed
+        // TODO add your handling code here:
+        double n1 = Double.parseDouble(txtn1.getText());
+        double n2 = Double.parseDouble(txtn2.getText());
+        double n3 = Double.parseDouble(txtn3.getText());
+        double p1=0;
+        p1= (n1+n2+n3) / 3;
+        txtprom.setText(Double.toString(p1));
+    }//GEN-LAST:event_btncActionPerformed
+
+    private void txtpromActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtpromActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtpromActionPerformed
+
+    private void btnlActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnlActionPerformed
+        // TODO add your handling code here:
+        txtn1.setText("");
+        txtn2.setText("");
+        txtn3.setText("");
+        txtprom.setText("");
+        txtn1.grabFocus();
+    }//GEN-LAST:event_btnlActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +190,16 @@ public class notas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnc;
+    private javax.swing.JButton btnl;
+    private javax.swing.JButton btnp;
+    private javax.swing.JButton btns;
+    private javax.swing.JLabel lbln1;
+    private javax.swing.JLabel lbln2;
+    private javax.swing.JLabel lbln3;
+    private javax.swing.JTextField txtn1;
+    private javax.swing.JTextField txtn2;
+    private javax.swing.JTextField txtn3;
+    private javax.swing.JTextField txtprom;
     // End of variables declaration//GEN-END:variables
 }
